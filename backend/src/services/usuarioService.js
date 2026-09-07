867c9a59fbf85cb74c160586bcd4788a78cb36a1
Lucas Muniz <lucas.muniz.ifpr@gmail.com>
2026-09-07 15:23:12 -0300

    feat(user): add regras de negocio do usuario

--- backend/src/services/usuarioService.js
+++ backend/src/services/usuarioService.js
@@ -1,7 +1,19 @@
 const Usuario = require('../models/Usuario');
 
-const obterTodosUsuarios = async () => {
-    return await Usuario.findAll();
+const obterTodosUsuarios = async => {
+    return Usuario.findAll();
 };
 
-module.exports = { obterTodosUsuarios }
+const criarUsuario = async (dados) => {
+    const novoUsuario = await Usuario.create(dados);
+    const usuarioSemSenha = novoUsuario.toJSON();
+    delete usuarioSemSenha.senha;
+    
+    return usuarioSemSenha;
+};
+
+module.exports = { 
+    obterTodosUsuarios,
+    criarUsuario,
+    removerUsuario
+};
